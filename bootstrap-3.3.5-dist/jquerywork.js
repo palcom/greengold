4739b9554f10b9e6f17db82cc1c915837e77f53e
paul <planetbeuty@gmail.com>
2015-11-03 16:27:46 +0300

    commit all the changes

---
+++ bootstrap-3.3.5-dist/jquerywork.js
@@ -0,0 +1,18 @@
+
+// check if the document has loaded
+$ (document) .ready(function()
+
+
+	{
+		$("h1") .mouseenter(function()
+		{
+				$(this).fadeout().fade out();
+
+				//$(this).slideUp(100).slideDown(100) ;
+
+	     }); //end
+
+		$("#project1 h5").hide() ;
+
+
+	});
---
+++ bootstrap-3.3.5-dist/js/jquerywork.js
@@ -0,0 +1,39 @@
+
+// check if the document has loaded
+$ (document) .ready(function()
+
+
+	{
+		$("h1") .mouseenter(function()
+		{
+				$(this).fadeOut().fadeIn();
+
+				//$(this).slideUp(100).slideDown(100) ;
+
+	     }); //end
+
+		$("#project1 h5").hide() ;
+		$("#project img").mouseenter(function()
+
+		{
+			$("#project1 h5").show();{
+			//$("#project1 h5").mouseleave(function(){
+
+		});
+
+		("#project1 h5").hide() ;
+		$("#project img").mouseenter(function()
+
+		("#project1").mouseleave(function()
+		("#project1").css("background-color", "gray");
+
+		{
+
+
+
+
+
+
+
+
+	});
